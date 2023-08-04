fc28c5c16c30c3f29ce052c8d2ebf02452853459
unreal-zorro <unrealzorro@gmail.com>
2023-08-04 07:36:12 +0300

    add react virtualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,11 +1,13 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ArticleList.module.scss';
-import { type FC, type HTMLAttributeAnchorTarget, memo } from 'react';
+import { type FC, type HTMLAttributeAnchorTarget, type LegacyRef, memo } from 'react';
 import { type Article, ArticleView } from '../../model/types/article';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import { ArticleListItemSkeleton } from 'entities/Article/ui/ArticleListItem/ArticleListItemSkeleton';
 import { Text, TextSize } from 'shared/ui/Text/Text';
 import { useTranslation } from 'react-i18next';
+import { List, type ListRowProps, WindowScroller } from 'react-virtualized';
+import { PAGE_ID } from 'widgets/Page/Page';
 
 interface ArticleListProps {
   className?: string;
@@ -33,15 +35,36 @@ export const ArticleList: FC<ArticleListProps> = memo((props: ArticleListProps) 
   } = props;
   const { t } = useTranslation('article-list');
 
-  const renderArticle = (article: Article) => {
+  const isBig = view === ArticleView.BIG;
+
+  const itemsPerRow = isBig ? 1 : 3;
+  const rowCount = isBig ? articles.length : Math.ceil(articles.length / itemsPerRow);
+
+  const rowRender = ({ index, key, style }: ListRowProps) => {
+    const items = [];
+    const fromIndex = index * itemsPerRow;
+    const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
+
+    for (let i = fromIndex; i < toIndex; i++) {
+      items.push(
+        <ArticleListItem
+          key={articles[i].id}
+          article={articles[i]}
+          view={view}
+          className={cls.card}
+          target={target}
+        />
+      );
+    }
+
     return (
-      <ArticleListItem
-        key={article.id}
-        article={article}
-        view={view}
-        className={cls.card}
-        target={target}
-      />
+      <div
+        key={key}
+        style={style}
+        className={cls.row}
+      >
+        {items}
+      </div>
     );
   };
 
@@ -54,12 +77,37 @@ export const ArticleList: FC<ArticleListProps> = memo((props: ArticleListProps) 
   }
 
   return (
-    <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-      {articles.length > 0
-        ? articles.map(renderArticle)
-        : null
+    <WindowScroller
+      scrollElement={document.getElementById(PAGE_ID) as Element}
+    >
+      {({
+        height,
+        width,
+        registerChild,
+        onChildScroll,
+        isScrolling,
+        scrollTop
+      }) =>
+        (
+          <div
+            ref={registerChild as LegacyRef<HTMLDivElement>}
+            className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+          >
+            <List
+              height={height ?? 700}
+              rowCount={rowCount}
+              rowHeight={isBig ? 700 : 330}
+              rowRenderer={rowRender}
+              width={width ? width - 80 : 700}
+              autoHeight
+              onScroll={onChildScroll}
+              isScrolling={isScrolling}
+              scrollTop={scrollTop}
+            />
+            { isLoading && getSkeletons(view) }
+          </div>
+        )
       }
-      { isLoading && getSkeletons(view) }
-    </div>
+    </WindowScroller>
   );
 });
