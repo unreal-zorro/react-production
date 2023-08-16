90e42170871c377c88cd280e15476059c79c8b74
unreal-zorro <unrealzorro@gmail.com>
2023-08-16 18:27:44 +0300

    add circular dependency plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import { type BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 
 export function buildPlugins ({ paths, isDev, apiUrl, project }: BuildOptions): webpack.WebpackPluginInstance[] {
   const plugins = [
@@ -25,6 +26,10 @@ export function buildPlugins ({ paths, isDev, apiUrl, project }: BuildOptions): 
       patterns: [
         { from: paths.locales, to: paths.buildLocales }
       ]
+    }),
+    new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true
     })
   ];
 
