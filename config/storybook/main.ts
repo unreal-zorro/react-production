c1282e1128cbff88e1ac8aef6f757a4a1fbeccbb
unreal-zorro <unrealzorro@gmail.com>
2023-08-22 20:17:25 +0300

    add aliases in storybook

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: [
     '../../src/**/*.stories.@(js|jsx|ts|tsx)'
@@ -9,12 +11,24 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     'storybook-addon-mock'
   ],
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
   },
   docs: {
     autodocs: 'tag'
+  },
+  webpackFinal: async (config) => {
+    if (config?.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../../src')
+      };
+    }
+    return config;
   }
 };
 export default config;
