cbad8fca102d7aedb0c5cd0ebfe95874ae813d00
unreal-zorro <unrealzorro@gmail.com>
2023-09-07 19:26:37 +0300

    skip one test, add stubs (fixtures)

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,9 +3,23 @@ import * as profileCommands from './commands/profile';
 import * as articleCommands from './commands/article';
 import * as commentsCommands from './commands/comments';
 import * as ratingCommands from './commands/rating';
+// import * as process from 'process';
 
 Cypress.Commands.addAll(commonCommands);
 Cypress.Commands.addAll(profileCommands);
 Cypress.Commands.addAll(articleCommands);
 Cypress.Commands.addAll(commentsCommands);
 Cypress.Commands.addAll(ratingCommands);
+// Cypress.Commands.overwrite('intercept', () => {
+//   const FIXTURE_MODE = process.env.FIXTURE_MODE;
+//   const fixtureName = req.METHOD + req.url + hash(req.body);
+//   if (FIXTURE_MODE === 'READ') {
+//     readFixture(fixtureName);
+//   }
+//   if (FIXTURE_MODE === 'WRITE') {
+//     createFixture(fixtureName, req.body);
+//   }
+//   if (FIXTURE_MODE === 'API') {
+//     // Прогон перед релизом
+//   }
+// });
