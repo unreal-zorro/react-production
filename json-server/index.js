8121b3f328bdd4e86c1014aee0758a55c3db33c5
unreal-zorro <unrealzorro@gmail.com>
2023-09-27 19:04:27 +0300

    add http server for development

--- json-server/index.js
+++ json-server/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
 const https = require('https');
+const http = require('http');
 
 const options = {
   key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
@@ -59,12 +60,16 @@ server.use((req, res, next) => {
 server.use(router);
 
 // Запуск сервера
-// server.listen(8000, () => {
-//   console.log('server is running on 8000 port');
-// });
-
 const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
 const PORT = 8443;
+const HTTP_PORT = 8000;
+
 httpsServer.listen(PORT, () => {
   console.log(`server is running on ${PORT} port`);
+});
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`server is running on ${HTTP_PORT} port`);
 });
